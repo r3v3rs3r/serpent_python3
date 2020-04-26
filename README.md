80ecdd68900fb77a7cd704077ce53160869cc341
jmp <test@test.test>
2020-04-26 15:03:10 +0300

    minor improvements

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # serpent_python3
+Serpent cipher ported to python 3. 
+
+ECB/CBC modes tests OK.
+
+Original sources from: https://github.com/yann2192/PySerpent
--- test.py
+++ test.py
@@ -8,34 +8,47 @@ import codecs
 
 if __name__ == '__main__':
     print("####### ECB ENCRYPT/DECRYPT #########")
-    ctx = _serpent(b"\xDE"*32)
-    buff = ctx.encrypt(b"\xDE"*16)
+    key = b"\xFF"*32
+    ctx = _serpent(key)
+    print ('key = ' + key.hex())
+    plaintext = b"\xDE"*16
+    buff = ctx.encrypt(plaintext)
+    print("Encrypted buffer:")
     print(buff.hex())
-
-    ctx = _serpent(b"\xDE"*32)
+    ctx = _serpent(key)
+    print("Decrypted buffer:")
     print(ctx.decrypt(buff).hex())
     print("--------------------------------")
     print("####### СBC ENCRYPT/DECRYPT #1 #########")
-    ctx = serpent_cbc(b"\x31\x32\x33\x34\x35\x36\x37\x38"+b"\0"*8, b"\0"*16) #if len = 8 we should add 8 zero bytes to the key (SERPENT key sizes: 128,192,256 bit)
-    #buff = ctx.encrypt("abcdefghijklmnopqrstuvwxyz0123456789")
-    buff = ctx.encrypt(b"\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7A\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39") #abcdefghijklmnopqrstuvwxyz0123456789
+    key = b"\x31\x32\x33\x34\x35\x36\x37\x38"+b"\0"*8  #if len = 8 we should add 8 zero bytes to the key (SERPENT key sizes: 128,192,256 bit)
+    iv = b"\0" * 16
+    print('key = ' + key.hex())
+    print('iv = ' + iv.hex())
+    ctx = serpent_cbc(key, iv)
+    #abcdefghijklmnopqrstuvwxyz0123456789
+    plaintext = b"\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7A\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39"
+    buff = ctx.encrypt(plaintext)
     print("Encrypted buffer:")
     print(buff.hex())
 
-    ctx = serpent_cbc(b"\x31\x32\x33\x34\x35\x36\x37\x38"+b"\0"*8, b"\0"*16)
+    ctx = serpent_cbc(key, iv)
     buff = ctx.decrypt(buff)
     print("Decrypted buffer:")
     print(buff.hex())
 
     print("--------------------------------")
     print("####### СBC ENCRYPT/DECRYPT #2 #########")
-    ctx = serpent_cbc(b"\x31\x32\x33\x34\x35\x36\x37\x38\x31\x32\x33\x34\x35\x36\x37\x38", b"\0"*16) #if len = 8 we should add 8 zero bytes to the key (SERPENT key sizes: 128,192,256 bit)
-    #buff = ctx.encrypt("abcdefghijklmnopqrstuvwxyz0123456789")
+    key = b"\x31\x32\x33\x34\x35\x36\x37\x38\x31\x32\x33\x34\x35\x36\x37\x38"  # if len = 8 we should add 8 zero bytes to the key (SERPENT key sizes: 128,192,256 bit)
+    iv = b"\x12"*16
+    ctx = serpent_cbc(key, iv)
+    print('key = ' + key.hex())
+    print('iv = ' + iv.hex())
+    # abcdefghijklmnopqrstuvwxyz0123456789
     buff = ctx.encrypt(b"\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7A\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39") #abcdefghijklmnopqrstuvwxyz0123456789
     print("Encrypted buffer:")
     print(buff.hex())
 
-    ctx = serpent_cbc(b"\x31\x32\x33\x34\x35\x36\x37\x38\x31\x32\x33\x34\x35\x36\x37\x38", b"\0"*16)
+    ctx = serpent_cbc(key, iv)
     buff = ctx.decrypt(buff)
     print("Decrypted buffer:")
     print(buff.hex())
